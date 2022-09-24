37e22c875fe4be33c6543b92c4611929aa1eb106
PanRoman1 <104255693+PanRoman1@users.noreply.github.com>
2022-09-24 18:59:30 +0000

    Deploying to gh-pages from @ PanRoman1/parcel-56-js@a5922ac6555e554ccad5652fb286b8f5ca3d3f49 🚀

---
+++ index.8faff814.js
@@ -0,0 +1,2 @@
+!function(){var o=fetchCountries=function(){console.log("fetch")};fetchCountries();var l;l="Hello bl world!!!",console.log(l),o(),console.log(330),console.log("Eazy"),console.log("js56")}();
+//# sourceMappingURL=index.8faff814.js.map
---
+++ index.929a7974.js
@@ -0,0 +1,2 @@
+var o=fetchCountries=()=>{console.log("fetch")};fetchCountries();var l;l="Hello bl world!!!",console.log(l),o(),console.log(330),console.log("Eazy"),console.log("js56");
+//# sourceMappingURL=index.929a7974.js.map
